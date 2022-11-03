68fe05e45067222287b2e94dcf4df2f100978116
Pranavsing <98748115+Pranavsing@users.noreply.github.com>
2022-11-03 13:52:42 +0530

    w1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TBD pranav name change update 2.0
-          new line added
-        </a>
+   <>
+   
+   <h1>TBD_PRNV</h1>
+   </>
+
       </header>
     </div>
   );
